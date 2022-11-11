2e6fd335c79efd37984269f7ae058fae1652ed5b
user <user@SBELPC>
2022-11-11 22:35:16 +0300

    Add main file

--- GraviSim/Source.cpp
+++ GraviSim/Source.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Gravity Simulator");
 
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
