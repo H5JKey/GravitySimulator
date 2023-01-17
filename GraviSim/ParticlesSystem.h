42625c7a27f20f6313663858b967b8c2e7b0893b
user <user@SBELPC>
2023-01-17 22:00:01 +0300

    Some Particles changes

--- GraviSim/ParticlesSystem.h
+++ GraviSim/ParticlesSystem.h
@@ -101,9 +101,9 @@ public:
         {
             // give a random velocity and lifetime to the particle
             float angle = (std::rand() % 360) * 3.14f / 180.f;
-            float speed = (std::rand() % 50) + 50.f;
+            float speed = (std::rand() % 70) + 50.f;
             m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-            m_particles[index].lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
+            m_particles[index].lifetime = sf::milliseconds((std::rand() % 1000) + 700);
 
             // reset the position of the corresponding vertex
             m_vertices[index].position = m_emitter;
