abc06a708a320c06c3430e6bb3866bf5c34fa881
user <user@SOKOLIKHPC>
2023-12-03 19:16:17 +0300

    added new objects properties

--- GraviSim/src/Simulation.cpp
+++ GraviSim/src/Simulation.cpp
@@ -458,9 +458,6 @@ void Simulation::updateGui() {
             ImGui::Checkbox("Affected by gravity", &newObj.properties.affectedByGravity);
             ImGui::Checkbox("Affectes others", &newObj.properties.affectsOthers);
             ImGui::Separator();
-            ImGui::Checkbox("Consider in gravity center", &newObj.useForGravityCenter);
-            ImGui::Checkbox("Affected by gravity", &newObj.properties.affectedByGravity);
-            ImGui::Checkbox("Affectes others", &newObj.properties.affectsOthers);
             if (ImGui::Button("Create"))
                 objects.push_back(newObj);
             ImGui::Separator();
